2f9c560414e6aec01cb8eafc682fc8528619f8a1
abattalov <abattalov14@gmail.com>
2022-08-29 16:32:42 -0500

    began adding routing, setup routing for home page and error page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home'
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
     <Router>
       <Navbar/>
-      <div className="App">
-        <h1> Hello</h1>
-      </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+      <Footer/>
     </Router>
     
   );
--- src/components/Course.js
+++ src/components/Course.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Course() {
   return (
     <div>
-        <h1>Welcome to my courses!</h1>
+        Course
     </div>
   )
 }
