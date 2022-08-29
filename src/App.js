38e3b3885badd728b55a3f9aa33f415ba4f499a8
abattalov <abattalov14@gmail.com>
2022-08-29 16:27:12 -0500

    made my component files, most of which are going to be static components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar/>
+      <div className="App">
+        <h1> Hello</h1>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Course.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Course() {
+  return (
+    <div>
+        <h1>Welcome to my courses!</h1>
+    </div>
+  )
+}
+
+export default Course
