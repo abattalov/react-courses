46d78c21f966eebab345ac89f41c643d1062d47a
abattalov <abattalov14@gmail.com>
2022-08-29 16:13:02 -0500

    first commit

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    courses: [],
+    loading: true
+}
+
+const courseReducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
